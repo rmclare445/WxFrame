4d941a2377c47f9714ff40552380e64463874ea9
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2022-05-01 02:35:38 -0700

    Update get_img.py
    Exception handling for image retrievals

--- wxframe/get_img.py
+++ wxframe/get_img.py
@@ -67,8 +67,10 @@ images = ( pw_conus_maxtemp, pw_conus_mintemp, pw_conus_24hrqpf,
 
 
 def get_img( url, filename ):
-    # Open the url image, set stream to True, this will return the stream content.
-    r = requests.get( url, stream = True )
+    try:
+        r = requests.get( url, stream = True )
+    except:
+        return
     # Check if the image was retrieved successfully
     if r.status_code == 200:
         # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
