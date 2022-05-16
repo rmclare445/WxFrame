783f4cf5e1230ff6d18f7ec99e3497de34abc807
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2022-05-16 02:01:22 -0700

    Update GOES and NEXRAD
    Include attributed for reading by page, add update cycling information (may be done away with later)

--- wxframe/goes.py
+++ wxframe/goes.py
@@ -8,20 +8,26 @@ import shutil
 import requests
 import config.locs
 
-goes_url = "https://whirlwind.aos.wisc.edu/~wxp/goes17/vis_color/westcoast/"
-goes_loc = config.locs.image_dir + "wc_goes\\"
-goes_lst = ["latest_westcoast_%s.jpg"%i for i in range(73)[1:]]
+name = 'GOES West Coast'
+cdir = -1                               # Cycling direction
+url  = "https://whirlwind.aos.wisc.edu/~wxp/goes17/vis_color/westcoast/"
+loc  = config.locs.image_dir + "wc_goes\\"
+lst  = ["latest_westcoast_%s.jpg"%i for i in range(73)[1:]]
 
-def get_goes( ):
-    for img in goes_lst:
+def get_goes( parent ):
+    parent.status_dialogue( 'UPDATING GOES...' )
+    parent.goes_update = True
+    for img in lst:
         # Open the url
-        r = requests.get( goes_url+img, stream = True )
+        r = requests.get( url+img, stream = True )
         # Check if the webpage was retrieved successfully
         if r.status_code == 200:
             r.raw.decode_content = True
-            filename = goes_loc + img
+            filename = loc + img
             with open(filename,'wb') as f:
                 shutil.copyfileobj(r.raw, f)
+    parent.goes_update = False
+    parent.rm_status_dialogue()
 
 if __name__ == "__main__":
     get_goes()
