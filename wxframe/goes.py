0ac1680d909a8fd7648c7f3c64bb264887cdb0ab
Ryan Clare <rmclare445@gmail.com>
2022-06-20 01:20:28 -0700

    Dash and nexrad/goes updates
     On branch image-updates
    
     Changes to be committed:
    	modified:   wxframe/dashboard.py
    	modified:   wxframe/goes.py
    	modified:   wxframe/nexrad.py
    	modified:   wxframe/wxframe.py

--- wxframe/goes.py
+++ wxframe/goes.py
@@ -14,9 +14,9 @@ url  = "https://whirlwind.aos.wisc.edu/~wxp/goes17/vis_color/westcoast/"
 loc  = img_dir + "wc_goes" + sep
 lst  = ["latest_westcoast_%s.jpg"%i for i in range(73)[1:]]
 
-def get_goes( parent ):
+def get_goes( ): #parent ):
     #parent.status_dialogue( 'UPDATING GOES...' )
-    parent.goes_update = True
+    #parent.goes_update = True
     for img in lst:
         # Open the url
         r = requests.get( url+img, stream = True )
@@ -26,7 +26,7 @@ def get_goes( parent ):
             filename = loc + img
             with open(filename,'wb') as f:
                 shutil.copyfileobj(r.raw, f)
-    parent.goes_update = False
+    #parent.goes_update = False
     #parent.rm_status_dialogue()
 
 if __name__ == "__main__":
