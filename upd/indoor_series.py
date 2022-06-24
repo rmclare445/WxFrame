09471f4c77b913018258732c964b99c13495d2ec
Ryan Clare <rmclare445@gmail.com>
2022-06-24 00:59:59 -0700

    Add indoor_series.py
     Changes to be committed:
    	new file:   indoor_series.py

---
+++ upd/indoor_series.py
@@ -0,0 +1,30 @@
+'''
+
+Record most recent thermopi retrieval to series
+
+'''
+
+
+def write_series( ):
+
+    with open("./log.thermopi", "r") as f:
+        current = f.read()
+
+    try:
+        with open("./log.series", "r") as f:
+            content = f.readlines()
+    except:
+        content = []
+
+    # Record entries from each minute up to 2 days out
+    content.append(current)
+    lim = min( len(content), 2880 )
+    content = content[-lim:]
+
+    with open("./log.series", "w") as f:
+        for line in content:
+            f.write( line )
+
+
+if __name__ == "__main__":
+    write_series()
